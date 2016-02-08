4db3ac9d96ce5418889a6bbbec4a301b4feca6eb
matsuis <matsuis@umich.edu>
2016-02-08 18:19:51 -0500

    Add camera

---
+++ Prototype_1/Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+    public GameObject hero;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 temp = Vector3.zero;
+        temp.x = hero.transform.position.x;
+        temp.y = hero.transform.position.y;
+        temp.z = -20;
+        transform.position = temp;
+        
+        //pos.y
+	}
+}

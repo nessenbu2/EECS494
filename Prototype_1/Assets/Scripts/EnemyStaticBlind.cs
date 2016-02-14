e95f0a8eb7cdc079f4bfb00ade1634f7d7f3c048
David A. Kolb <dakbox10@gmail.com>
2016-02-14 18:24:17 -0500

    Trying the fix the revert fuck up.

---
+++ Prototype_1/Assets/Scripts/EnemyStaticBlind.cs
@@ -0,0 +1,10 @@
+﻿public class EnemyStaticBlind : EnemyBase
+{
+    // This virtual function fires this enemy's weapon.
+    // Here in EnemyStaticBlind, this fires in a random direction.
+    protected override void fire()
+    {
+        randomShot();
+        return;
+    }
+}
